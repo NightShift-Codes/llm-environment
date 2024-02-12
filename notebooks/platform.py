f99dc5c7030143d1c779a0b59c6881667127211c
Matt Proetsch <mattproetsch@gmail.com>
2024-02-11 19:00:38 -0500

    formatting + remove unused globals

--- notebooks/platform.py
+++ notebooks/platform.py
@@ -13,11 +13,15 @@ else:
 
 dtype = torch.bfloat16
 if device == "mps":
-    has_bf16 = int(Popen(["/bin/sh", "-c", "sysctl hw | grep FEAT_BF16 | awk '{print $2}'"], stdout=PIPE)
-                   .stdout
-                   .read()
-                   .decode("ascii")
-                   .strip())
+    has_bf16 = int(
+        Popen(
+            ["/bin/sh", "-c", "sysctl hw | grep FEAT_BF16 | awk '{print $2}'"],
+            stdout=PIPE,
+        )
+        .stdout.read()
+        .decode("ascii")
+        .strip()
+    )
     if not has_bf16:
         print("Using M1 (no bfloat16 support - falling back to float16)")
         dtype = torch.float16
@@ -26,6 +30,9 @@ if device == "mps":
 
 print(f"Platform: {platform.platform()} ({sys.platform} on {platform.machine()})")
 if device == "cpu":
-    print("WARNING: No accelerator detected. Models may take a long time to run!", file=sys.stderr)
+    print(
+        "WARNING: No accelerator detected. Models may take a long time to run!",
+        file=sys.stderr,
+    )
 else:
     print(f"Using {device} to accelerate model inference")
