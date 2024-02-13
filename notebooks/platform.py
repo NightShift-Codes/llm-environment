beddd05faa821736f4f5131a8d1428b24bcd24ce
Matt Proetsch <mattproetsch@gmail.com>
2024-02-13 01:06:00 -0500

    Intel Macs don't report BFLOAT_16

--- notebooks/platform.py
+++ notebooks/platform.py
@@ -4,7 +4,7 @@ import sys
 import torch
 
 
-if torch.backends.mps.is_available():  # MacOS with M1, M2, or M3 chip
+if torch.backends.mps.is_available():  # MacOS
     device = "mps"
 elif torch.cuda.is_available():  # Linux or Windows running in Docker
     device = "cuda"
@@ -13,20 +13,24 @@ else:
 
 dtype = torch.bfloat16
 if device == "mps":
-    has_bf16 = int(
-        Popen(
-            ["/bin/sh", "-c", "sysctl hw | grep FEAT_BF16 | awk '{print $2}'"],
-            stdout=PIPE,
+    try:
+        has_bf16 = int(
+            Popen(
+                ["/bin/sh", "-c", "sysctl hw | grep FEAT_BF16 | awk '{print $2}'"],
+                stdout=PIPE,
+            )
+            .stdout.read()
+            .decode("ascii")
+            .strip()
         )
-        .stdout.read()
-        .decode("ascii")
-        .strip()
-    )
-    if not has_bf16:
-        print("Using M1 (no bfloat16 support - falling back to float16)")
+        if not has_bf16:
+            print("No bfloat16 support on M1 - falling back to float16")
+            dtype = torch.float16
+        else:
+            print("Using bfloat16 on M2/M3")
+    except:
+        print("No bfloat16 support on Intel - falling back to float16")
         dtype = torch.float16
-    else:
-        print("Using bfloat16 on M2/M3")
 
 print(f"Platform: {platform.platform()} ({sys.platform} on {platform.machine()})")
 if device == "cpu":
